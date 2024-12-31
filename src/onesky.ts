3da68c98c38c036ae7f246ecaf5bc671544e28ef
Ricardo Devis Agullo <mkredz@gmail.com>
2024-12-31 09:29:55 +0100

    lint more

--- src/onesky.ts
+++ src/onesky.ts
@@ -3,83 +3,83 @@ import { z } from "zod";
 import type { LanguageInfo } from "./fetch-translations";
 
 const OneSkyLanguageInfo = z.object({
-  is_ready_to_publish: z.boolean(),
-  code: z.string(),
-  english_name: z.string(),
-  local_name: z.string(),
+	is_ready_to_publish: z.boolean(),
+	code: z.string(),
+	english_name: z.string(),
+	local_name: z.string(),
 });
 type OneSkyLanguageInfo = z.infer<typeof OneSkyLanguageInfo>;
 const OneSkyLanguageResponse = z.object({
-  data: z.array(OneSkyLanguageInfo),
+	data: z.array(OneSkyLanguageInfo),
 });
 
 const TranslationSchema = z.record(z.union([z.string(), z.record(z.string())]));
 type TranslationSchema = z.infer<typeof TranslationSchema>;
 
 interface TranslationFile {
-  [languageCode: string]: TranslationSchema;
+	[languageCode: string]: TranslationSchema;
 }
 
 export interface ProjectTranslations {
-  [fileName: string]: TranslationFile;
+	[fileName: string]: TranslationFile;
 }
 
 export interface OneSky {
-  getLanguages: (opts: {
-    apiKey: string;
-    secret: string;
-    projectId: number;
-  }) => Promise<OneSkyLanguageInfo[]>;
-  getFile: (opts: {
-    apiKey: string;
-    secret: string;
-    projectId: number;
-    fileName: string;
-    languages: LanguageInfo[];
-  }) => Promise<TranslationFile>;
+	getLanguages: (opts: {
+		apiKey: string;
+		secret: string;
+		projectId: number;
+	}) => Promise<OneSkyLanguageInfo[]>;
+	getFile: (opts: {
+		apiKey: string;
+		secret: string;
+		projectId: number;
+		fileName: string;
+		languages: LanguageInfo[];
+	}) => Promise<TranslationFile>;
 }
 
 export const getLanguages: OneSky["getLanguages"] = async ({
-  apiKey,
-  secret,
-  projectId,
+	apiKey,
+	secret,
+	projectId,
 }) => {
-  try {
-    const response = await onesky.getLanguages({ secret, apiKey, projectId });
-    const parsed = OneSkyLanguageResponse.parse(JSON.parse(response));
+	try {
+		const response = await onesky.getLanguages({ secret, apiKey, projectId });
+		const parsed = OneSkyLanguageResponse.parse(JSON.parse(response));
 
-    return parsed.data;
-  } catch (err) {
-    throw Error("Error getting OneSky language info");
-  }
+		return parsed.data;
+	} catch (err) {
+		throw Error("Error getting OneSky language info");
+	}
 };
 
 export const getFile: OneSky["getFile"] = async ({
-  apiKey,
-  secret,
-  projectId,
-  fileName,
-  languages,
+	apiKey,
+	secret,
+	projectId,
+	fileName,
+	languages,
 }) => {
-  try {
-    const obj: Record<string, TranslationSchema> = {};
-    for (const { code } of languages) {
-      console.log("Fetching file:", fileName, "for locale:", code);
+	try {
+		const obj: Record<string, TranslationSchema> = {};
+		for (const { code } of languages) {
+			console.log("Fetching file:", fileName, "for locale:", code);
 
-      const response = await onesky.getFile({
-        secret,
-        apiKey,
-        projectId,
-        fileName,
-        language: code,
-      });
+			const response = await onesky.getFile({
+				secret,
+				apiKey,
+				projectId,
+				fileName,
+				language: code,
+			});
 
-      const parsed = TranslationSchema.parse(JSON.parse(response));
-      obj[code] = parsed;
-    }
+			const parsed = TranslationSchema.parse(JSON.parse(response));
+			obj[code] = parsed;
+		}
 
-    return obj;
-  } catch (err) {
-    throw Error(`Error getting OneSky translation: ${JSON.stringify(err)}`);
-  }
+		return obj;
+	} catch (err) {
+		throw Error(`Error getting OneSky translation: ${JSON.stringify(err)}`);
+	}
 };
--- test/oneSkyNock.ts
+++ test/oneSkyNock.ts
@@ -3,119 +3,119 @@ import nock from "nock";
 import type { FetchTranslationsConfiguration } from "../src/fetch-translations";
 
 const languageData = {
-  meta: { status: 200, record_count: 2 },
-  data: [
-    {
-      code: "en-GB",
-      english_name: "English (United Kingdom)",
-      local_name: "English (United Kingdom)",
-      custom_locale: null,
-      locale: "en",
-      region: "GB",
-      is_base_language: true,
-      is_ready_to_publish: true,
-      translation_progress: "100.0%",
-      last_updated_at: "2020-12-09T16:13:01+0000",
-      last_updated_at_timestamp: 1607530381,
-    },
-    {
-      code: "pt-PT",
-      english_name: "Portuguese (Portugal)",
-      local_name: "Português (Europeu)",
-      custom_locale: null,
-      locale: "pt",
-      region: "PT",
-      is_base_language: false,
-      is_ready_to_publish: true,
-      translation_progress: "91.7%",
-      last_updated_at: "2020-12-09T16:41:50+0000",
-      last_updated_at_timestamp: 1607532110,
-    },
-  ],
+	meta: { status: 200, record_count: 2 },
+	data: [
+		{
+			code: "en-GB",
+			english_name: "English (United Kingdom)",
+			local_name: "English (United Kingdom)",
+			custom_locale: null,
+			locale: "en",
+			region: "GB",
+			is_base_language: true,
+			is_ready_to_publish: true,
+			translation_progress: "100.0%",
+			last_updated_at: "2020-12-09T16:13:01+0000",
+			last_updated_at_timestamp: 1607530381,
+		},
+		{
+			code: "pt-PT",
+			english_name: "Portuguese (Portugal)",
+			local_name: "Português (Europeu)",
+			custom_locale: null,
+			locale: "pt",
+			region: "PT",
+			is_base_language: false,
+			is_ready_to_publish: true,
+			translation_progress: "91.7%",
+			last_updated_at: "2020-12-09T16:41:50+0000",
+			last_updated_at_timestamp: 1607532110,
+		},
+	],
 };
 
 const translationData = {
-  "main.json": {
-    "en-GB": {
-      hello: "Hello",
-    },
-    "pt-PT": {
-      hello: "Olá",
-    },
-  },
-  "errors.json": {
-    "pt-PT": {
-      failure: "falha falha",
-    },
-    "en-GB": {
-      failure: "Failure",
-    },
-  },
+	"main.json": {
+		"en-GB": {
+			hello: "Hello",
+		},
+		"pt-PT": {
+			hello: "Olá",
+		},
+	},
+	"errors.json": {
+		"pt-PT": {
+			failure: "falha falha",
+		},
+		"en-GB": {
+			failure: "Failure",
+		},
+	},
 };
 type Filename = keyof typeof translationData;
 type LanguageFileName = keyof (typeof translationData)[Filename];
 
 const getDevHash = (secret: string, timestamp: number): string =>
-  md5(String(timestamp) + secret);
+	md5(String(timestamp) + secret);
 
 const nockOneSky = () => nock("https://platform.api.onesky.io/");
 
 export const nockLanguages = (cfg: {
-  secret: string;
-  projectId: number;
-  apiKey: string;
+	secret: string;
+	projectId: number;
+	apiKey: string;
 }): nock.Scope =>
-  nockOneSky()
-    .get(`/1/projects/${cfg.projectId}/languages`)
-    .query((obj) => {
-      return (
-        obj.api_key === cfg.apiKey &&
-        !!Number(obj.timestamp) &&
-        obj.dev_hash === getDevHash(cfg.secret, Number(obj.timestamp))
-      );
-    })
-    .reply(200, languageData);
+	nockOneSky()
+		.get(`/1/projects/${cfg.projectId}/languages`)
+		.query((obj) => {
+			return (
+				obj.api_key === cfg.apiKey &&
+				!!Number(obj.timestamp) &&
+				obj.dev_hash === getDevHash(cfg.secret, Number(obj.timestamp))
+			);
+		})
+		.reply(200, languageData);
 
 export const nockFile = (cfg: {
-  projectId: number;
-  secret: string;
-  fileName: string;
-  apiKey: string;
+	projectId: number;
+	secret: string;
+	fileName: string;
+	apiKey: string;
 }): void => {
-  for (const { code } of languageData.data) {
-    nockOneSky()
-      .get(`/1/projects/${cfg.projectId}/translations`)
-      .query((obj) => {
-        return (
-          Object.keys(translationData).includes(cfg.fileName) &&
-          obj.source_file_name === cfg.fileName &&
-          obj.api_key === cfg.apiKey &&
-          obj.locale === code &&
-          !!Number(obj.timestamp) &&
-          obj.dev_hash === getDevHash(cfg.secret, Number(obj.timestamp))
-        );
-      })
-      .reply(
-        200,
-        translationData[cfg.fileName as Filename][code as LanguageFileName]
-      );
-  }
+	for (const { code } of languageData.data) {
+		nockOneSky()
+			.get(`/1/projects/${cfg.projectId}/translations`)
+			.query((obj) => {
+				return (
+					Object.keys(translationData).includes(cfg.fileName) &&
+					obj.source_file_name === cfg.fileName &&
+					obj.api_key === cfg.apiKey &&
+					obj.locale === code &&
+					!!Number(obj.timestamp) &&
+					obj.dev_hash === getDevHash(cfg.secret, Number(obj.timestamp))
+				);
+			})
+			.reply(
+				200,
+				translationData[cfg.fileName as Filename][code as LanguageFileName],
+			);
+	}
 };
 
 export const nockProject = (config: FetchTranslationsConfiguration): void => {
-  for (const project of config.projects) {
-    nockLanguages({
-      apiKey: config.apiKey,
-      secret: config.secret,
-      projectId: project.id,
-    });
-    for (const file of project.files) {
-      nockFile({
-        apiKey: config.apiKey,
-        projectId: project.id,
-        fileName: file,
-        secret: config.secret,
-      });
-    }
-  }
+	for (const project of config.projects) {
+		nockLanguages({
+			apiKey: config.apiKey,
+			secret: config.secret,
+			projectId: project.id,
+		});
+		for (const file of project.files) {
+			nockFile({
+				apiKey: config.apiKey,
+				projectId: project.id,
+				fileName: file,
+				secret: config.secret,
+			});
+		}
+	}
 };
