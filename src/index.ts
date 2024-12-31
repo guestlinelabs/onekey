3ea6322ec057c7341068dc89bba6eff526caeeb2
Ricardo Devis Agullo <mkredz@gmail.com>
2024-12-31 21:53:37 +0100

    Export more stuff

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export { fetchTranslations } from "./fetch-translations";
 export { generateKeys } from "./generate-translation-keys";
-export { saveKeys, saveOneSkyTranslations } from "./file";
+export { translate } from "./translate";
+export {
+  saveKeys,
+  saveOneSkyTranslations,
+  saveAiTranslations,
+  upload,
+} from "./file";
+export { checkTranslations } from "./check-translations";
