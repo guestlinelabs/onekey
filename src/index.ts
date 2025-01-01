76a1bfe49df93b19ac19e133baafedf7444ac64b
Ricardo Devis Agullo <mkredz@gmail.com>
2025-01-01 09:21:19 +0100

    lint

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ export { fetchTranslations } from "./fetch-translations";
 export { generateKeys } from "./generate-translation-keys";
 export { translate } from "./translate";
 export {
-  saveKeys,
-  saveOneSkyTranslations,
-  saveAiTranslations,
-  upload,
+	saveKeys,
+	saveOneSkyTranslations,
+	saveAiTranslations,
+	upload,
 } from "./file";
 export { checkTranslations } from "./check-translations";
